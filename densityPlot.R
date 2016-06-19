97f686764a345f2e1b311b546548748a51f2a5d2
upendrak <upendra_35@yahoo.com>
2016-06-19 15:11:07 -0700

    add customized functions

---
+++ densityPlot.R
@@ -0,0 +1,29 @@
+#' Density plot of all samples
+#'
+#' Estimation the counts density for each sample
+#'
+#' @param counts \code{matrix} of counts
+#' @param group factor vector of the condition from which each sample belongs
+#' @param col colors of the curves (one per biological condition)
+#' @param outfile TRUE to export the figure in a png file
+#' @return A file named densplot.png in the figures directory
+#' @author Marie-Agnes Dillies and Hugo Varet
+
+densityPlot <- function(counts, group=target[,varInt], OutDir, col){
+    nd = paste(OutDir,"figures",sep="/")
+    
+    col <- unlist(strsplit(ret.opts$colors, ","))
+
+    png(filename=paste(nd,"densplot.png",sep="/"),width=1800,height=1800,res=300)
+    counts <- removeNull(counts)
+    plot(density(log2(counts[,1]+1)), las = 1, lwd = 2,
+         main = "Density of counts distribution",
+	     xlab = expression(log[2] ~ (raw ~ count + 1)),
+	     ylim = c(0,max(apply(counts,2,function(x){max(density(log2(x+1))$y)}))*1.05),
+         col = col[as.integer(group)[1]])
+    for (i in 2:ncol(counts)){
+      lines(density(log2(counts[,i]+1)),col=col[as.integer(group)[i]],lwd=2)
+    }
+  legend("topright", levels(group), lty=1, col=col[1:nlevels(group)], lwd=2, bty="n")
+  dev.off()
+}
---
+++ descriptionPlots.r
@@ -0,0 +1,43 @@
+#' Description plots of the counts
+#'
+#' Description plots of the counts according to the biological condition
+#'
+#' @param counts \code{matrix} of counts
+#' @param group factor vector of the condition from which each sample belongs
+#' @param col colors for the plots (one per biological condition)
+#' @return PNG files in the "figures" directory and the matrix of the most expressed sequences
+#' @author Hugo Varet
+
+descriptionPlots <- function(counts, n=3, group=target[,varInt], OutDir, col){
+  # create the figures directory if does not exist
+  dir.create(paste(OutDir,"figures",sep="/"), showWarnings=FALSE)
+  
+  source("/Users/upendrakumardevisetty/Documents/git_repos/edgeR_multifactorial/barplotTotal.R")
+
+  # total number of reads per sample
+  barplotTotal(counts, group=target[,varInt], OutDir, col)
+  
+  source("/Users/upendrakumardevisetty/Documents/git_repos/edgeR_multifactorial/barplotNull.R")
+
+  # percentage of null counts per sample
+  barplotNull(counts, group=target[,varInt], OutDir, col)
+  
+  source("/Users/upendrakumardevisetty/Documents/git_repos/edgeR_multifactorial/densityPlot.R")
+
+  # distribution of counts per sample
+  densityPlot(counts, group=target[,varInt], OutDir, col)
+  
+  source("/Users/upendrakumardevisetty/Documents/git_repos/edgeR_multifactorial/majSequences.R")
+
+  # features which catch the most important number of reads
+  majSequences <- majSequences(counts,  n=3, group=target[,varInt], OutDir, col)
+  
+  source("/Users/upendrakumardevisetty/Documents/git_repos/edgeR_multifactorial/pairwiseScatterPlots.R")
+
+  # SERE and pairwise scatter plots
+  cat("Matrix of SERE statistics:\n")
+  print(tabSERE(counts))
+  pairwiseScatterPlots(counts, group=target[,varInt], OutDir)
+  
+  return(majSequences)
+}
