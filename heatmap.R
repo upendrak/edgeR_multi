67790d40ba0388078d86c7a8771947ea603eea83
upendrak <upendra_35@yahoo.com>
2016-06-21 17:00:05 -0700

    add heatmap script

---
+++ heatmap.R
@@ -0,0 +1,24 @@
+Heatmap <- function(OutDir) 
+{
+		object=out.edgeR$dge
+
+		# normalization
+		dge <- calcNormFactors(object, method=normalizationMethod)
+		
+		# countspermillion 
+		countspermi <- cpm(dge, normalized.lib.sizes=TRUE)
+
+		# Now pick the genes with the top variance over all samples:
+		rv <- rowVars(countspermi)
+		idx <- order(-rv)[1:40]
+
+		# color scheme
+		# hmcol <- colorRampPalette(brewer.pal(9, "GnBu"))(100)
+		
+		nd = paste(OutDir,"figures",sep="/")
+
+		# Plotting
+  		png(filename=paste(nd,"heatmap.png",sep="/"),width=1800,height=1800,res=300)
+		heatmap(countspermi[idx,])
+		dev.off()
+}
